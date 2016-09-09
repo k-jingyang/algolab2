cb1c72a5d7490c5eb98710ac807a110cdf1f26e7
dotodin <wenchong.lee@gmail.com>
2016-09-09 22:08:26 +0800

    Completed functionalities
    Timers added
    -Runs through 50 loops and finds the average time taken for each hash
    types
    Source added
    -1000 names added

--- App.java
+++ App.java
@@ -13,101 +13,73 @@ public class App {
       HashTable hashTable1 = new HashTable(1000);
       HashTable hashTable2 = new HashTable(1000);
       long startTime = System.nanoTime();
-   
-      try {
+      int count=50;        //number of loops
+      int searchInt= 555;  //key to be searched
+      int timeTaken1=0;
+      int timeTaken2=0;
+      int timeTaken=0;
       
-         br = new BufferedReader(new FileReader(csvFile));
-         while ((line = br.readLine()) != null) {
-            // use comma as separator
-            String[] list = line.split(cvsSplitBy);
-           // System.out.println("Namelist [id= " + list[0] + ", name= " + list[1] + "]");
-            hashTable1.insertChainHashing(Integer.parseInt(list[0]), list[1]);
-         
-         }
-      
-      } 
-      catch (FileNotFoundException e) {
-         e.printStackTrace();
-      } 
-      catch (IOException e) {
-         e.printStackTrace();
-      } 
-      finally {
-         if (br != null) {
-            try {
-               br.close();
-            } 
-            catch (IOException e) {
-               e.printStackTrace();
+      for(int i =0; i <count; i ++){
+         hashTable1.clearTable();
+         hashTable2.clearTable();
+         try {
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+               String[] list = line.split(cvsSplitBy);
+               startTime = System.nanoTime();
+               hashTable1.insertChainHashing(Integer.parseInt(list[0]), list[1]);
+               timeTaken += (System.nanoTime()-startTime);
             }
          }
-      }
-      System.out.println("Chain Hashing set-up time= " +((System.nanoTime()-startTime)/1000) + " microseconds");
-      startTime = System.nanoTime();
-   
-      try {
-      
-         br = new BufferedReader(new FileReader(csvFile));
-         while ((line = br.readLine()) != null) {
-            // use comma as separator
-            String[] list = line.split(cvsSplitBy);
-           // System.out.println("Namelist [id= " + list[0] + ", name= " + list[1] + "]");
-            hashTable2.insertDoubleHashing(Integer.parseInt(list[0]), list[1]);
-         
+         catch (FileNotFoundException e) {e.printStackTrace();}
+         catch (IOException e) { e.printStackTrace(); } 
+         finally {
+            if (br != null) {
+               try { br.close(); }  
+               catch (IOException e) {e.printStackTrace(); }}
          }
-      
-      } 
-      catch (FileNotFoundException e) {
-         e.printStackTrace();
-      } 
-      catch (IOException e) {
-         e.printStackTrace();
-      } 
-      finally {
-         if (br != null) {
-            try {
-               br.close();
-            } 
-            catch (IOException e) {
-               e.printStackTrace();
+         timeTaken1+=timeTaken;
+         //System.out.println("Chain Hashing set-up time= " +timeTaken/1000 + " microseconds");
+         timeTaken =0;
+         try {
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+               String[] list = line.split(cvsSplitBy);
+               startTime = System.nanoTime();
+               hashTable2.insertDoubleHashing(Integer.parseInt(list[0]), list[1]);
+               timeTaken += (System.nanoTime()-startTime);
             }
          }
+         catch (FileNotFoundException e) {e.printStackTrace();}
+         catch (IOException e) { e.printStackTrace(); } 
+         finally {
+            if (br != null) {
+               try { br.close(); }  
+               catch (IOException e) {e.printStackTrace(); }}
+         }
+         timeTaken2+=timeTaken;
+         //System.out.println("Double Hashing set-up time= " +timeTaken/1000 + " microseconds");
+         timeTaken=0;
+               
       }
-      System.out.println("Chain Hashing set-up time= " +((System.nanoTime()-startTime)/1000) + " microseconds");
-      startTime = System.nanoTime();
-      System.out.println(hashTable1.searchChainHashing(504)); 
-      System.out.println("Chain Hashing search time= " +((System.nanoTime()-startTime)/1000) + " microseconds");
-      startTime = System.nanoTime();
-     System.out.println(hashTable1.searchDoubleHashing(505)); // <---error line
-      System.out.println("Double Hashing search time= " +((System.nanoTime()-startTime)/1000) + " microseconds");
-
-      //hashTable2.printHashTable();
-       //System.out.println("Printing Table:");
-      //hashTable1.printChainHashing();
-   /*
-      System.out.println("CHAINED HASHING");
-      System.out.println("===============");
-      HashTable hashTable1 = new HashTable(1000);
-      hashTable1.insertChainHashing(1,"Jing Yang");
-      hashTable1.insertChainHashing(2,"Test");
-      hashTable1.insertChainHashing(504,"Hello");
-      System.out.println("Printing Table:");
-      hashTable1.printChainHashing();
-      System.out.println("Searching Table:");
-      System.out.println(hashTable1.searchChainHashing(504));
-   
-      System.out.println();
-   
-      System.out.println("DOUBLE HASHING");
-      System.out.println("==============");
-      HashTable hashTable2 = new HashTable(1000);
-      hashTable2.insertDoubleHashing(1,"Jing Yang");
-      hashTable2.insertDoubleHashing(2,"MOMOMO");
-      hashTable2.insertDoubleHashing(504,"HELLLOOOO");
-      System.out.println("Printing Table:");
-      hashTable2.printHashTable();
-      System.out.println("Searching Table:");
-      System.out.println(hashTable2.searchDoubleHashing(504));*/
+      timeTaken1/=count;
+      timeTaken2/=count;
+      System.out.println("Chain Hashing set-up time=\t " +timeTaken1/1000 + " MICROseconds");
+      System.out.println("Double Hashing set-up time=\t " +timeTaken2/1000 + " MICROseconds");
+      timeTaken = timeTaken1 = timeTaken2 = 0;
+      String chain, doubleh;
+      for(int i =0; i <count; i ++){
+         startTime = System.nanoTime();
+         chain = hashTable1.searchChainHashing(searchInt);
+         timeTaken1 += (System.nanoTime()-startTime);
+         startTime = System.nanoTime();
+         doubleh = hashTable2.searchDoubleHashing(searchInt);
+         timeTaken2 += (System.nanoTime()-startTime);
+      }
+      timeTaken1/=count;
+      timeTaken2/=count;
+      System.out.println("Chain Hashing search time=\t " +timeTaken1 + " NANOseconds");
+      System.out.println("Double Hashing search time=\t " +timeTaken2+ " NANOseconds");
    }
 
 }
--- HashTable.java
+++ HashTable.java
@@ -1,111 +1,110 @@
 //Class for hash table
-
+import java.util.Arrays;
 public class HashTable {
-    private HashTableSlot slots[];
+   private HashTableSlot slots[];
 
-    public HashTable(int size){
-        slots = new HashTableSlot[size];
-        //for(int i = 0; i<size; i++){
-        //    slots[i] = new HashTableSlot();
-        //}
-    }
+   public HashTable(int size){
+      slots = new HashTableSlot[size];
+      //for(int i = 0; i<size; i++){
+      //    slots[i] = new HashTableSlot();
+      //}
+   }
+   public void clearTable(){
+      Arrays.fill(slots, null);
+   }
+   public HashTableSlot[] getHashTable(){
+      return this.slots;
+   }
 
-    public HashTableSlot[] getHashTable(){
-        return this.slots;
-    }
+   //Does not print the chained keys and values
+   public void printHashTable(){
+      for(int i = 0; i< slots.length; i++){
+         if(slots[i]!=null){
+            System.out.println(slots[i].getKey() + "-" + slots[i].getValue() + "  ");
+         }
+      }
+   }
 
-    //Does not print the chained keys and values
-    public void printHashTable(){
-        for(int i = 0; i< slots.length; i++){
-            if(slots[i]!=null){
-                System.out.println("index["+ i + "]"+slots[i].getKey() + "-" + slots[i].getValue() + "  ");
+   //Below are Double Hashing functions
+
+   //Returns 1 if successful, returns 0 if failure
+   public int insertDoubleHashing(int key, String value){
+      int start = HashMapFunction.hashMap(key);
+      int hashIncr = HashMapFunction.incrHash(key);
+      int index = start;
+      while(slots[index]!=null){
+         index = HashMapFunction.reHash(index, hashIncr);
+         if(key == 505){
+         }
+         if(index == start){
+            return 0;
+         }
+      }
+      slots[index] = new HashTableSlot(key, value);
+      return 1;
+   }
+
+   public String searchDoubleHashing(int key){
+      int start = HashMapFunction.hashMap(key);
+      int hashIncr = HashMapFunction.incrHash(key);
+      int index = start;
+      while(slots[index].getKey()!=key){
+         index = HashMapFunction.reHash(index, hashIncr);
+         if(index == start){
+            return null;
+         }
+      }
+      return slots[index].getValue();
+   }
+
+
+
+   //Below arer Chain Hashing functions
+
+   //Returns 1 if successful, returns 0 if failure
+   public int insertChainHashing(int key, String value){
+      int index = HashMapFunction.hashMap(key);
+      if(slots[index] == null){
+         slots[index] = new HashTableSlotChain(key, value, null);
+      }
+      else{
+         HashTableSlotChain p = (HashTableSlotChain)slots[index];
+         while(p.getNext()!=null){
+            p = p.getNext();
+         }
+         p.setNext(new HashTableSlotChain(key, value, null));
+         return 1;
+      }
+      return 0;
+   }
+
+   public String searchChainHashing(int key){
+      int index = HashMapFunction.hashMap(key);
+      if(slots[index].getKey()==key){
+         return slots[index].getValue();
+      }
+      else{
+         HashTableSlotChain p = ((HashTableSlotChain)slots[index]).getNext();
+         while(p!=null){
+            if(p.getKey() == key){
+               return p.getValue();
             }
-        }
-    }
+            p = p.getNext();
+         }
+      }
+      return null; 
+   }
 
-    //Below are Double Hashing functions
-
-    //Returns 1 if successful, returns 0 if failure
-    public int insertDoubleHashing(int key, String value){
-        int start = HashMapFunction.hashMap(key);
-        int hashIncr = HashMapFunction.incrHash(key);
-        int index = start;
-        while(slots[index]!=null){
-            index = HashMapFunction.reHash(index, hashIncr);
-            if(key == 505){
-            System.out.println("index = " + index);
-            }
-            if(index == start){
-                return 0;
-            }
-        }
-        slots[index] = new HashTableSlot(key, value);
-        return 1;
-    }
-
-    public String searchDoubleHashing(int key){
-        int start = HashMapFunction.hashMap(key);
-        int hashIncr = HashMapFunction.incrHash(key);
-        int index = start;
-        while(slots[index].getKey()!=key){
-            index = HashMapFunction.reHash(index, hashIncr);
-            
-       // System.out.println("index = " +index + " " + key);
-            if(index == start){
-                return null;
-            }
-        }
-        return slots[index].getValue();
-    }
-
-
-
-    //Below arer Chain Hashing functions
-
-    //Returns 1 if successful, returns 0 if failure
-    public int insertChainHashing(int key, String value){
-        int index = HashMapFunction.hashMap(key);
-        if(slots[index] == null){
-            slots[index] = new HashTableSlotChain(key, value, null);
-        }
-        else{
-            HashTableSlotChain p = (HashTableSlotChain)slots[index];
-            while(p.getNext()!=null){
-                p = p.getNext();
-            }
-            p.setNext(new HashTableSlotChain(key, value, null));
-            return 1;
-        }
-        return 0;
-    }
-
-    public String searchChainHashing(int key){
-        int index = HashMapFunction.hashMap(key);
-        if(slots[index].getKey()==key){
-            return slots[index].getValue();
-        }
-        else{
-            HashTableSlotChain p = ((HashTableSlotChain)slots[index]).getNext();
-            while(p!=null){
-                if(p.getKey() == key){
-                    return p.getValue();
-                }
-                p = p.getNext();
-            }
-        }
-        return null; 
-    }
-
-    public void printChainHashing(){
-        for(int i = 0; i< slots.length; i++){
-            HashTableSlotChain p = (HashTableSlotChain)slots[i];
-            while(p!=null){
-                System.out.print(p.getKey() + "-" + p.getValue() + "  ");
-                p = p.getNext();
-            }
-            if(slots[i]!=null){
-                System.out.println();
-            }
-        }
-    }
+   public void printChainHashing(){
+      for(int i = 0; i< slots.length; i++){
+         HashTableSlotChain p = (HashTableSlotChain)slots[i];
+         while(p!=null){
+            System.out.print(p.getKey() + "-" + p.getValue() + "  ");
+            p = p.getNext();
+         }
+         if(slots[i]!=null){
+            System.out.println();
+         }
+      }
+   }
 }
