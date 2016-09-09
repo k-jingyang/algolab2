9095cc7fc07a813b9866e91b8e576b98590bdcd8
Jing Yang <k.jingyang@gmail.com>
2016-09-09 15:55:32 +0800

    Implemented chain hashing

--- App.java
+++ App.java
@@ -2,6 +2,9 @@ public class App {
     public static void main(String[] args){
         HashTable hashTable = new HashTable(1000);
         hashTable.chainHashingInsert(1,"Jing Yang");
+        hashTable.chainHashingInsert(2,"Test");
+        hashTable.chainHashingInsert(504,"Kheng");
+        hashTable.printChainHashingTable();
     }
 
 }
--- HashTable.java
+++ HashTable.java
@@ -18,9 +18,31 @@ public class HashTable {
         
     }
 
-    public void chainHashingInsert(int key, String value){
-        HashTableSlotChain slotChain  = new HashTableSlotChain(key, value, null);
-        slots[0] = slotChain;
-        System.out.println(((HashTableSlotChain) slots[0]).getValue());
+    public int chainHashingInsert(int key, String value){
+        int start = HashMapFunction.hashMap(key);
+        int index = start;
+        if(slots[index] == null){
+            slots[index] = new HashTableSlotChain(key, value, null);
+        }
+        else{
+            HashTableSlotChain p = (HashTableSlotChain)slots[index];
+            while(p.getNext()!=null){
+                p = p.getNext();
+            }
+            p.setNext(new HashTableSlotChain(key, value, null));
+            return 1;
+        }
+        return 0;
+    }
+
+    public void printChainHashingTable(){
+        for(int i = 0; i< slots.length; i++){
+            HashTableSlotChain p = (HashTableSlotChain)slots[i];
+            while(p!=null){
+                System.out.print(p.getKey() + "-" + p.getValue() + "  ");
+                p = p.getNext();
+            }
+            System.out.println();
+        }
     }
 }
