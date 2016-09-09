b8df15296add4b4d291e67b35f27a062fe4c389b
dotodin <wenchong.lee@gmail.com>
2016-09-09 23:01:35 +0800

    Sizes addition
    Observation:
    -Time for first run will always take longer than others, regardless of
    size.

--- App.java
+++ App.java
@@ -5,6 +5,14 @@ import java.io.IOException;
 
 public class App {
    public static void main(String[] args){
+      testRun(100, 50);
+      testRun(300, 150);
+      testRun(500, 250);
+   //   testRun(700, 350);
+    //  testRun(1000, 555);
+   }
+   
+   public static void testRun(int size, int searchInt){
       String csvFile = System.getProperty("user.dir") + "\\namelist.csv";
       BufferedReader br = null;
       String line = "";
@@ -12,23 +20,25 @@ public class App {
    
       HashTable hashTable1 = new HashTable(1000);
       HashTable hashTable2 = new HashTable(1000);
-      long startTime = System.nanoTime();
+      long startTime;
       int count=50;        //number of loops
-      int searchInt= 555;  //key to be searched
+      //int searchInt= 355;  //key to be searched
       int timeTaken1=0;
       int timeTaken2=0;
       int timeTaken=0;
-      
+      int j=0;
       for(int i =0; i <count; i ++){
          hashTable1.clearTable();
          hashTable2.clearTable();
          try {
             br = new BufferedReader(new FileReader(csvFile));
-            while ((line = br.readLine()) != null) {
+            while ((line = br.readLine()) != null && j<size) {
+            
                String[] list = line.split(cvsSplitBy);
                startTime = System.nanoTime();
                hashTable1.insertChainHashing(Integer.parseInt(list[0]), list[1]);
                timeTaken += (System.nanoTime()-startTime);
+               j++;
             }
          }
          catch (FileNotFoundException e) {e.printStackTrace();}
@@ -38,16 +48,18 @@ public class App {
                try { br.close(); }  
                catch (IOException e) {e.printStackTrace(); }}
          }
+         j=0;
          timeTaken1+=timeTaken;
          //System.out.println("Chain Hashing set-up time= " +timeTaken/1000 + " microseconds");
          timeTaken =0;
          try {
             br = new BufferedReader(new FileReader(csvFile));
-            while ((line = br.readLine()) != null) {
+            while ((line = br.readLine()) != null && j<size) {
                String[] list = line.split(cvsSplitBy);
                startTime = System.nanoTime();
                hashTable2.insertDoubleHashing(Integer.parseInt(list[0]), list[1]);
                timeTaken += (System.nanoTime()-startTime);
+               j++;
             }
          }
          catch (FileNotFoundException e) {e.printStackTrace();}
@@ -57,13 +69,16 @@ public class App {
                try { br.close(); }  
                catch (IOException e) {e.printStackTrace(); }}
          }
+         j=0;
          timeTaken2+=timeTaken;
          //System.out.println("Double Hashing set-up time= " +timeTaken/1000 + " microseconds");
          timeTaken=0;
                
+      //hashTable1.printChainHashing();
       }
       timeTaken1/=count;
       timeTaken2/=count;
+      System.out.println("Time stats for " +size + " sized data set, with search term " +searchInt);
       System.out.println("Chain Hashing set-up time=\t " +timeTaken1/1000 + " MICROseconds");
       System.out.println("Double Hashing set-up time=\t " +timeTaken2/1000 + " MICROseconds");
       timeTaken = timeTaken1 = timeTaken2 = 0;
@@ -81,5 +96,4 @@ public class App {
       System.out.println("Chain Hashing search time=\t " +timeTaken1 + " NANOseconds");
       System.out.println("Double Hashing search time=\t " +timeTaken2+ " NANOseconds");
    }
-
 }
